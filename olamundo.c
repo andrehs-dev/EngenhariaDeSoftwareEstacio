326274c79c3698d2129e4935191839465e5966d8
André Henrique <andrehs.dev@gmail.com>
2026-03-31 22:04:42 -0300

    Comment out initial repository description

--- olamundo.c
+++ olamundo.c
@@ -1,2 +1,9 @@
-# EngenhariaDeSoftwareEstacio
-Neste repositório estarei atualizando meus trabalhos e exercícios que estarei fazendo na faculdade.
+// # EngenhariaDeSoftwareEstacio
+// Neste repositório estarei atualizando meus trabalhos e exercícios que estarei fazendo na faculdade.
+
+#include <stdio.h>
+
+int main() {
+  printf("Olá, Mundo!\n");
+  return 0;
+}
